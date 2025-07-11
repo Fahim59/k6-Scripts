827c2a5b247b7a05b85220833f1205a7ac8c958c
Fahim59 <mrahaman59@yahoo.com>
2025-07-11 17:54:38 +0600

    tc6 updated

--- Test Cases/TC6.js
+++ Test Cases/TC6.js
@@ -1,6 +1,7 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 import { SharedArray } from 'k6/data';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 const searchTerms = new SharedArray('searchValues', function () {
   return [
@@ -91,4 +92,12 @@ export default function (data) {
   });
 
   sleep(1);
+}
+
+export function handleSummary(data) {
+  return {
+    "tc1.html": htmlReport(data),
+    'tc1_result-summary.json': JSON.stringify(data),
+    //k6 run script.js --out csv = tc_1result.csv
+  };
 }
